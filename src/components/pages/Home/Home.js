b4cae8852f6f52e2528c31380983765e58d4ffbf
rianyunandar <rianyunandar@gmail.com>
2022-02-06 15:42:26 +0700

    seo basic

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
+
 import {
   Services,
   // Services3,
   // Portfolio2,
   Slider,
   Features2,
-  Contacts2
+  Contacts2,
 } from "../../widgets";
 import { useCustomState } from "../../../state/state";
 
@@ -14,6 +16,15 @@ export default () => {
 
   return (
     <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>poles lantai indonesia</title>
+        <link rel="canonical" href="http://polesindo.com/" />
+        <meta
+          name="keywords"
+          content="jasa poles lantai, poles lantai, poles lantai permeter, tukang poles lantai, harga poles lantai , harga poles lantai permeter, jasa poles lantai lampung, jasa poles lantai palembang, jasa poles lantai jabotabek"
+        />
+      </Helmet>
       <Slider data={state.data.posts} />
       {/* <Services3 data={state.data.salepoint} /> */}
       <Features2 data={state.data.features2} />
